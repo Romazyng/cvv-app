232dd5f49ed6429172997fb3288a9b5ff166c831
Roman Chernetsky <misteryo.wins@gmail.com>
2025-05-24 11:47:44 +0300

    vercel changes

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import FormField from './FormField'
-import { Button } from './button'
+// import React from 'react'
+// import FormField from './FormField'
+// import { Button } from './button'
 
 export default function Form({}) {
 
