e37de0f85af8d8509273c3f8d655c9df6e7d3ec6
Roman Chernetsky <misteryo.wins@gmail.com>
2025-05-24 11:49:42 +0300

    TROLLING

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="w-full bg-orange-600 h-12 flex items-center justify-center rounded-b-2xl shadow-2xl">
       <h1 className="text-white uppercase text-xl font-extrabold">
-        cv creator
+        vika - cherni noob 
       </h1>
     </header>
   )
